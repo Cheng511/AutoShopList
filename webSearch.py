431143d415136ceff5b7b630bca123eee853aa09
JC <jc@JC-2.local>
2021-03-11 16:00:24 +0800

    PCHome24hr Scrapper done

--- webSearch.py
+++ webSearch.py
@@ -1,18 +1,23 @@
 # coding = utf-8
+# PChome24h 爬蟲
 from selenium import webdriver
 import requests
 import json
 import urllib.parse
 from bs4 import BeautifulSoup
-from selenium.webdriver.chrome.options import Options
+import csv
+import datetime
 
 keyword = str(input("請輸入搜尋關鍵字: "))
+n = int(input("請問要搜尋多少頁的內容? "))
 parse_word = urllib.parse.quote(keyword)
+print('搜尋結果如下: ')
+print('='*30)
 
 
 def Get_Results(parse_word):
     # 這個函式會針對輸入的搜尋關鍵字取出PCHome24hr搜尋結果前5頁資料的標題
-    for i in range(0, 1):
+    for i in range(0, n-1):
         response = requests.get(
             'https://ecshweb.pchome.com.tw/search/v3.3/all/results?q=' + parse_word + '&page=' + str(i) + '&sort=sale/dc')
         raw_data = response.content.decode('utf-8')
@@ -22,31 +27,19 @@ def Get_Results(parse_word):
             for key in products:
                 product_name = key['name']
                 prod_id = key['Id']
-                if product_name != '' or prod_id != '':
-                    print(prod_id + '\n' + product_name + '\n' +
-                          'current price at: $' + Get_Current_Price(prod_id) + '\n')
-                elif product_name == '':
-                    print(prod_id + '\n' + 'No Product Name' + '\n' +
-                          'current price at: $' + Get_Current_Price(prod_id) + '\n')
-                elif prod_id == '':
-                    print('No product ID' + '\n' + 'No Product Name' + '\n' +
-                          'current price at: $' + Get_Current_Price(prod_id) + '\n')
-                    print()
+                prod_price = str(key['price'])
+                link = 'https://24h.pchome.com.tw/prod/' + prod_id
+                print('名稱: ' + product_name + '\n' +
+                      '價格: $' + prod_price + '\n' +
+                      '網址: https://24h.pchome.com.tw/prod/' + prod_id + '\n')
+                print()
+                today = str(datetime.date.today())
+                with open(today + '_' + keyword + '.csv', 'a') as f:
+                    file = csv.writer(f)
+                    file.writerow(['名稱', '價格', '網址'])
+                    file.writerow([product_name, prod_price, link])
         except Exception as err:
             print(str(err))
-
-
-def Get_Current_Price(prod_id):
-    # 此函式會針對輸入的產品Id給出當前價錢
-    driver = webdriver.Chrome('/Users/jc/chromedriver')
-    driver.get('https://24h.pchome.com.tw/prod/' + prod_id)
-    try:
-        element = driver.find_element_by_css_selector('#PriceTotal')
-        current_price = element.text
-        driver.close()
-        return str(current_price)
-    except Exception as err:
-        print(str(err))
 
 
 Get_Results(parse_word)
